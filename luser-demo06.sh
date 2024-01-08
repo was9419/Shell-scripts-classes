fdd090428b9e395060ca13ba598e1574c876fc18
was9419 <47761851+was9419@users.noreply.github.com>
2024-01-07 18:03:47 -0600

    Update luser-demo06.sh

--- luser-demo06.sh
+++ luser-demo06.sh
@@ -26,3 +26,9 @@ do
 	PASSWORD=$(date +%s%N | sha256sum | head -c48)
 	echo "$[USER_NAME]: ${PASSWORD}"
 done
+# generate and display a password for each parameter.
+for USER_NAME in "${@}"
+do
+	PASSWORD=$(date +%s%N | sha256sum | head -c48)
+	echo "$[USER_NAME]: ${PASSWORD}"
+done
