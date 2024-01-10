9fdc9dc869e1561586daa99fc8b6735bcfb0c8e4
was9419 <47761851+was9419@users.noreply.github.com>
2024-01-09 18:56:06 -0600

    Test Resolution

--- add-new-local-user.sh
+++ add-new-local-user.sh
@@ -1,38 +1,68 @@
 #!/bin/bash
+#
+# This script creates a new user on the local system.
+# You must supply a username as an argument to the script.
+# Optionally, you can also provide a comment for the account as an argument.
+# A password will be automatically generated for the account.
+# The username, password, and host for the account will be displayed.
 
-# Check if script is running with ROOT privileges.
-if [[ "$[UID]" -ne 0 ]]
+# Make sure the script is being executed with superuser privileges.
+if [[ "${UID}" -ne 0 ]]
 then
-	echo "Please run with sudo or as root."
-	exit 1 
-fi 
-# If no paraneters are share while running the sctipt fail and exit 1 
-if [[ "${#}" -lt 2 ]]
-then 
-	echo "No paraneters were shared while running the script."
-	exit 1 
+   echo 'Please run with sudo or as root.'
+   exit 1
 fi
-echo
-# Shows parameters 
-echo "Username: $1"
-echo "Name: $2"
-HOSTNAME=$(hostname -s)
-# Creates user in the system 
-adduser -c "${2}" -m "${1}" 
-PASSWORD=$(date +%s%N${RANDOM})
-echo "${PASSWORD}" | passwd --stdin ${1}
-passwd -e ${1}
-echo
-# Informs if the user was sucessfully created.
-if [[ adduser -eq 0 ]]
+
+# If the user doesn't supply at least one argument, then give them help.
+if [[ "${#}" -lt 1 ]]
 then
-	echo "User ${1} was sucessfully created."
-else
-	echo "User creation failed, please try again"
-	exit
+  echo "Usage: ${0} USER_NAME [COMMENT]..."
+  echo 'Create an account on the local system with the name of USER_NAME and a comments field of COMMENT.'
+  exit 1
 fi
+
+# The first parameter is the user name
+USER_NAME="${1}"
+
+# The rest of the parameters are for the account comments.
+shift
+COMMENT="${@}"
+
+# Generate a password.
+PASSWORD=$(date +%s%N | sha256sum | head -c48)
+
+# Create the user with the password.
+useradd -c "${COMMENT}" -m ${USER_NAME}
+
+# Check to see if the useradd command succeeded.
+# We don't want to tell the user that an account was created when it hasn't been.
+if [[ "${?}" -ne 0 ]]
+then
+  echo 'The account could not be created.'
+  exit 1
+fi
+
+# Set the password.
+echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+
+# Check to see if the passwd command succeeded.
+if [[ "${?}" -ne 0 ]]
+then
+  echo 'The password for the account could not be set.'
+  exit 1
+fi
+
+# Force password change on first login.
+passwd -e ${USER_NAME}
+
+# Display the username, password, and the host where the user was created.
 echo
-# Display information (username, password and host) to share with staff 
-echo "Username: ${1}"
-echo "Password: ${PASSWORD}"
-echo "Hostname: ${HOSTNAME}"
+echo 'username:'
+echo "${USER_NAME}"
+echo
+echo 'password:'
+echo "${PASSWORD}"
+echo
+echo 'host:'
+echo "${HOSTNAME}"
+exit 0
