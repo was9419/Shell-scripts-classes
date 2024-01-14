9f42adb4a3e40f63f02ce002900556543227681b
was9419 <47761851+was9419@users.noreply.github.com>
2024-01-13 21:51:38 -0600

    Classs 7 script

---
+++ luser-demo07.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Display the first three parameters.
+echo "Parameter ${1}"
+echo "Parameter ${2}"
+echo "Parameter ${3}"
+echo 
+
+# Loop through all the positional parameters.
+while [[ "${#}" -gt 0 ]]
+do
+	echo "Number of parameters: ${#}"
+	echo "Parameter 1: ${1}"
+	echo "Parameter 2: ${2}"
+	echo "Parameter 3: ${3}"
+	echo
+done
+shift
