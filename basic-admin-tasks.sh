64b9f30d31a577ac5c14a32be5c9866377046844
was9419 <47761851+was9419@users.noreply.github.com>
2024-02-08 18:51:50 -0600

    Update basic-admin-tasks.sh

--- basic-admin-tasks.sh
+++ basic-admin-tasks.sh
@@ -1,1 +1,18 @@
-.
+#!/bin/bash
+# Author: Wilfredo Santamaria 
+# Creation date 02/08/2024
+# This script will run basic admin task
+echo
+echo "This script will run basic administrative commands" 
+echo
+top | head -10
+echo
+df -h
+echo
+free -m
+echo
+uptime
+echo
+iostat
+echo
+echo "End of Script" 
